f665c5db928cbee7d52179035b311942a6c52eff
Duy Anh <ndanhdev@gmail.com>
2023-07-25 15:25:38 +0700

    Cai dat express chay thong bao xin chao

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
+// Enable CORS for all routes
 app.use(cors());
+
+// Parse JSON requests
 app.use(express.json());
+
+// Root route handler
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to contact book application." });
+  res.json({ message: "Welcome to the contact book application." });
 });
+
+// Export the app to be used in other files
 module.exports = app;
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const app = require("./app");
 const config = require("./app/config");
-// start server
-const PORT = config.app.port;
-app.listen(PORT, () => {
+
+// Start the server
+const PORT = config.app.port || 3000; // Use port from config or default to 3000
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error("Error starting server:", err);
+  } else {
     console.log(`Server is running on port ${PORT}.`);
+  }
 });
