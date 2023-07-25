42172e0bb1b10b7358649a875df200bd64777d05
Duy Anh <ndanhdev@gmail.com>
2023-07-25 17:39:49 +0700

    findAll

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -16,3 +16,19 @@ exports.create = async (req, res, next) => {
         return next(new ApiError(500, 'An error occurred while creating the contact'));
     }
 };
+
+exports.findAll = async (req, res, next) => {
+    let documents = [];
+    try {
+        const contactService = new ContactService(MongoDB.client);
+        const { name } = req.query;
+        if (name) {
+            documents = await contactService.findByName(name);
+        } else {
+            documents = await contactService.find({});
+        }
+    } catch (error) {
+        return next(new ApiError(500, 'An error occurred while retrieving contacts'));
+    }
+    return res.send(documents);
+};
--- app/services/contact.service.js
+++ app/services/contact.service.js
@@ -25,5 +25,32 @@ class ContactService {
         );
         return result.value;
     }
+    async find(filter) {
+        const cursor = await this.Contact.find(filter);
+        return await cursor.toArray();
+    }
+    async findByName(name) {
+        return await this.find({
+            name: { $regex: new RegExp(name), $options: 'i' },
+        });
+    }
+    async find(filter) {
+        const cursor = await this.Contact.find(filter);
+        return await cursor.toArray();
+    }
+    async findByName(name) {
+        return await this.find({
+            name: { $regex: new RegExp(name), $options: 'i' },
+        });
+    }
+    async find(filter) {
+        const cursor = await this.Contact.find(filter);
+        return await cursor.toArray();
+    }
+    async findByName(name) {
+        return await this.find({
+            name: { $regex: new RegExp(name), $options: 'i' },
+        });
+    }
 }
 module.exports = ContactService;
