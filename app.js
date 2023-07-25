125a6b490aaa9fbfb914fd182e6673c8a317a20a
Duy Anh <ndanhdev@gmail.com>
2023-07-25 15:35:38 +0700

    git commit -m "Dinh nghia cac route cho tai nguyen Contact"

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const contactsRouter = require("./app/routes/contact.route");
 
 // Enable CORS for all routes
 app.use(cors());
@@ -12,6 +13,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the contact book application." });
 });
+app.use("/api/contacts", contactsRouter);
 
 // Export the app to be used in other files
 module.exports = app;
